509c7f46c0a5a92310dcb4c1ceb2990a8e42e97b
Benony Gabriel <gbenn.work@gmail.com>
2024-12-17 01:31:17 +0700

    Add blog page and update routes for blogs, about, and contact

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/blogs', function () {
+    return view('blogs');
+})->name('blogs');
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
