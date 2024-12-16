18e651088b79804c83da53fa50fe688a9299bdec
Benony Gabriel <gbenn.work@gmail.com>
2024-12-17 01:52:36 +0700

    Refactor page templates to use Blade components for consistent layout structure

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,15 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Finsweet | About</title>
-</head>
-
-<body>
-    <h1>About</h1>
-</body>
-
-</html>
+<x-layout>
+    <section>
+        <h1>About</h1>
+    </section>
+</x-layout>
