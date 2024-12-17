0ca487a920488056622384a4f9814022c8f52ecc
Benony Gabriel <gbenn.work@gmail.com>
2024-12-17 13:39:38 +0700

    Update NavLink component styles for improved accessibility and visual consistency

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,3 +1,3 @@
 <a {{ $attributes }}
-    class="{{ $active ? 'text-md font-semibold text-white pb-1 border-b-2 border-yellow-500' : 'text-gray-800 md:text-white text-md hover:text-gray-400 duration-300' }}"
+    class="{{ $active ? 'text-black md:text-white font-semibold pb-1 border-b-2 border-yellow-500' : 'text-gray-800 md:text-white text-md hover:text-gray-400 duration-300' }}"
     aria-current="{{ $active ? 'page' : false }}">{{ $slot }}</a>
